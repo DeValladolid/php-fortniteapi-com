ad770df6d9e9da2d9f5c920cda0804fe9b8d215e
srdrabx <devalladolidoficial@gmail.com>
2019-02-21 12:08:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-require_once 'fortnite-api/Autoloader.php';
-
-$api = new FortniteClient;
-
-$data = $api->status->fetch();
-
-echo $data->status;
-echo $data->message;
-echo $data->version;
-?>
-
-<?php
 
 $curl = curl_init();
 
