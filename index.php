5eacb6d27161488d038048dc5549cca618e71327
srdrabx <devalladolidoficial@gmail.com>
2019-02-21 12:07:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,3 +9,33 @@ echo $data->status;
 echo $data->message;
 echo $data->version;
 ?>
+
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://fortnite-public-api.theapinetwork.com/prod09/br_motd/get",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => false,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => array('language' => 'en'),
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: your_api_key"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+} ?>
